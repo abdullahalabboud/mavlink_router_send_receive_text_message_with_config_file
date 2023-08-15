6c34ade97f423672e724fb3b971ca2d87fdc4282
abdullahalabboud <alabboudabdullah289@gmail.com>
2023-08-15 12:42:55 +0300

    commit1

---
+++ receiver.py
@@ -0,0 +1,45 @@
+import sys
+import time
+from pymavlink import mavutil
+
+
+from time import sleep
+
+OWN_COMP_ID = 1
+
+# python3 examples/receiver.py 127.0.0.1:4000 50
+
+if len(sys.argv) != 3:
+    print("Usage : %s <ip:udp_port> <system_id>" % (sys.argv[0]))
+    print(
+        "Receive mavlink heartbeats on specified interface. "
+        "Respond with a ping message"
+    )
+    sys.exit()
+
+
+srcSystem = int(sys.argv[2])
+
+mav = mavutil.mavlink_connection(
+    "udpin:" + sys.argv[1], source_system=srcSystem, source_component=OWN_COMP_ID
+)
+mav.wait_heartbeat()
+while True:
+    msg = mav.recv_match(blocking=True)
+    print("Message from %d %s " % (msg.get_srcSystem(), msg))
+    if hasattr(msg, "target_system"):
+        if msg.target_system == 0:
+            print("\tMessages send to all ")
+        elif msg.target_system == srcSystem:
+            print("\tMessages send to me ")
+        else:
+            print("\tMessages send to other")
+    else:
+        print("\tMessages without target system")
+
+    # mav.mav.ping_send(
+    #     int(time.time() * 1000), msg.seq, msg.get_srcSystem(), msg.get_srcComponent()
+    # )
+    # mav.mav.status_text(mavutil.mavlink.MAV_SEVERITY_INFO, msg)
+
+    sleep(1)
---
+++ sender.py
@@ -0,0 +1,53 @@
+import sys
+from pymavlink import mavutil
+import time
+
+from threading import Thread
+from time import sleep
+
+
+TARGET_COMP_ID = 1
+# python3 examples/sender.py 127.0.0.1:3000 100 0
+# argv[3] : means 0 value and that has used in ping_send method to indicates a PING request
+
+if len(sys.argv) != 4:
+    print("Usage : %s <ip:udp_port> <system_id> <target_system_id>" % (sys.argv[0]))
+    print(
+        "Send mavlink pings, using given <system-id> and <target-system-id>, "
+        "to specified interface"
+    )
+    sys.exit()
+
+
+mav = mavutil.mavlink_connection("udp:" + sys.argv[1], source_system=int(sys.argv[2]))
+# print("mav try connection")
+mav.wait_heartbeat()
+# print("mav wait heartbeat ")
+
+
+def pingloop():
+    i = 0
+    while True:
+        # print("start while pind send")
+        mav.mav.statustext_send(
+            mavutil.mavlink.MAV_SEVERITY_INFO, "text message".encode()
+        )
+
+        # mav.mav.ping_send(
+        #     int(time.time() * 1000),
+        #     i,
+        #     int(sys.argv[3]),
+        #     TARGET_COMP_ID,
+        # )
+
+
+# print("try threading ")
+pingthread = Thread(target=pingloop)
+pingthread.daemon = True
+pingthread.start()
+# print("start threading ")
+
+
+while True:
+    msg = mav.recv_match(blocking=True)
+    print("Message from %d: %s:" % (msg.get_srcSystem(), msg))
